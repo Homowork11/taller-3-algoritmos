96ce2eca1c41ebb83b3973f60fcc39897424d9bf
ana sofia <anasofiangel05@gmail.com>
2025-05-24 14:30:43 -0500

    primer commit

---
+++ while2.js
@@ -0,0 +1,30 @@
+let advertencia = 2;//declara una variable y asigna un valor 
+let limiteDeAdvertencias = 3;//declara una variable y asigna un valor
+let contadorDeDuchas = 0;//declara una variable y asigna un valor
+
+ 
+console.log("Sistema de monitoreo de ahorro de agua");
+console.log("Se registrarán los tiempos de ducha. El sistema terminará cuando las advertencias alcancen el límite.\n");
+while (advertencia <limiteDeAlvertencias){
+    console.log ("ingrese el tiempo de ducha en segundos:");
+if(let,entrada,tiempo)
+  contadorDeDuchas++;
+if (tiempo < 180){
+    console.log("ahorro registrado en la ducha{0}.¡buen trabajo!/n,cotador de duchas")
+}
+else
+{
+    advertencia++;
+    console.log(`¡Advertencia! La ducha {contadorDeDuchas} superó los 180 segundos. Advertencias: {advertencia}\n`);
+}
+} 
+console.log("Se alcanzó el límite de advertencias. Fin del monitoreo.");
+
+
+
+
+
+
+
+
+
